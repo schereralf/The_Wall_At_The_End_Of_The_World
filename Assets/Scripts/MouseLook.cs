363ebde0e92402eece04d325144fc705c17794ad
schereralf <31801379+schereralf@users.noreply.github.com>
2022-06-28 17:40:58 +0200

    New Controller Installed, first tests
    Includes functioning new character controller

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -1,27 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 50f;
     public Transform playerBody;
     float xRotation = 0f;
-
 
     // Start is called before the first frame update
     void Start()
     {
+        //Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+    // Update is called once per frame - this just deals with camera view and how it responds to mouse movements.
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -70f, 70f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         playerBody.Rotate(Vector3.up * mouseX);
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private readonly float speed = 7.5f;
+    private readonly float jumpSpeed = 3.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public CharacterController controller;
+    private Vector3 move;
+    private Vector3 velocity;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
+    public GameObject bow;
+    private bool canShoot;
+    float arrowReloadTime;
+
+    // Update is called once per frame.  Here we do basic movement including jumps.
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the player Forward  
+
+        move = transform.right * horizontalInput + transform.forward * forwardInput;
+        controller.Move(speed * Time.deltaTime * move);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpSpeed * -2f * gravity);
+        }
+
+        if (Input.GetMouseButton(0) && canShoot)
+        {
+            arrowReloadTime = Time.time + 0.2f;
+            canShoot = false;
+            // get object from the pool
+            GameObject pooledProjectile = ObjectPooler.SharedInstance.GetPooledObject();
+            if (pooledProjectile != null)
+            {
+                pooledProjectile.SetActive(true); 
+                // activate arrow
+                pooledProjectile.transform.SetPositionAndRotation(bow.transform.position, bow.transform.rotation);
+                // position the arrow in line with player
+            }
+        }
+
+        if (!canShoot && Time.time > arrowReloadTime) 
+            // wait a while after last arrow was shot
+            canShoot = true;
+    }
+}
